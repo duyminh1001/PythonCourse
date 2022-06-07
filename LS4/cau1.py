7686aedc5f56fcf79478febb7109cd200e827b3c
duyminh1001 <105925058+duyminh1001@users.noreply.github.com>
2022-06-07 13:56:12 +0700

    Add files via upload

---
+++ LS4/cau1.py
@@ -0,0 +1,23 @@
+vanlist= ["sinh nhat",11,7,2003]
+#vanlist.append("sinh nhat ban")
+
+#vanlist.insert(0, "sinh ngay")
+
+nalist = [13,9]
+#vanlist.extend(nalist)
+#mylist = vanlist + nalist
+
+tuyetlist=[" NGAY 6/4"]
+vanlist.extend(tuyetlist)
+
+#x=vanlist.index(7)
+
+vanlist.remove('sinh nhat')
+
+vanlist.pop(-1)
+
+#vanlist.sort()
+
+#vanlist.sort(reverse = True)
+
+print(vanlist)
---
+++ LS4/cau2.py
@@ -0,0 +1,7 @@
+vanlist= ['apple','banana','cherry','orange','kiwi','melon','mango']
+#x=vanlist[:2]
+#y= vanlist[-2:]
+
+vanlist.remove("mango")
+vanlist.pop(0)
+print(vanlist)
